2f5b0b0929d00bb702d32d267c332e1c8ab48c76
Devansh Desai <ddesai14@uncc.edu>
2018-10-29 18:59:17 -0400

    a

--- models/employeeModel.js
+++ models/employeeModel.js
@@ -150,7 +150,120 @@ var EmployeeSchema = new mongoose.Schema({
     },  
 
     tasks : [{
-        type : Object
+
+        status : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true
+        }, 
+        startDate : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true
+        },
+        dueDate : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true
+        },
+		completedDate : {
+            type : String,
+            minlength : 1,
+            trim : true
+        },
+		endDate : {
+            type : String,
+            minlength : 1,
+            trim : true
+        },
+        title : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true  
+        },
+        instructions : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true
+        },rewardPoints : {
+            type : Number,
+            default : null
+        },
+        priority : {
+            type : Number,
+            required : true,
+            default : 0
+        },
+        
+        duration : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true
+        },
+        url : {
+            type : String,
+            trim : true
+        },
+        color : {
+            type : String,
+            default : "#e828e9"
+        },
+        category : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true
+        },
+        isComplete : {
+            type : Boolean,
+            default : false
+        },
+        inputs : [{
+            name : {
+                type : String
+            },
+            value : {
+                type : Boolean,
+                default : false
+            }
+        }],
+        
+        
+        
+        // for example: administrative, communication, generic, specific, departmental, travel
+        // Business will classify these
+        type : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true  
+        },
+    
+        location : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true  
+        },
+        department : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true  
+        },
+        division : {
+            type : String,
+            required : true,
+            minlength : 1,
+            trim : true  
+        }
+
     }],
     quizzes : [{
 
