6c7e07f40173f404f144165436044e866b6e298d
SpeedyProgrammerX <devanshdesai29@gmail.com>
2018-11-19 23:04:22 -0500

    I suck

--- Routes/file.js
+++ Routes/file.js
@@ -218,11 +218,3 @@ function validateQuestionJson(jsonItem, row) {
         majorError = false;
     }
 }
-
-
-var server = app.listen(5050, function () {
-    var host = server.address().address
-    var port = server.address().port
-    console.log("Server started at http://%s:%s", host, port)
- 
- })
