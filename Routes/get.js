45d927d918ecca0270423bb3153647d90d18259c
SpeedyProgrammerX <devanshdesai29@gmail.com>
2018-11-25 01:10:05 -0500

    Cc

--- Routes/get.js
+++ Routes/get.js
@@ -46,39 +46,44 @@ router.get('/getDashInfo', authenticateHR, function (req, res) {
             res.status(400).send(response)
         }else{
             var response = []
-            for(var i = 0; i < employees.length;i++){ response.push({}) }
+            for(var i = 0; i < employees.length;i++){ 
+				response.push({}) 
+			}
 
+			for(var i = 0; i < employees.length;i++){
+				response[i].jobTitle = employees[i].jobTitle
+				// response[i].reportTo = employees[i].reportTo
+				// response[i].isPeerBuddy = employees[i].isPeerBuddy
+				response[i]._id = employees[i]._id
+				response[i].firstName = employees[i].firstName
+				response[i].lastName = employees[i].lastName
+				// response[i].email = employees[i].email
+				// response[i].workPhone = employees[i].workPhone
+				response[i].totalTasks = employees[i].tasks.length
+				var completedTasks = 0
+				var overDueTasks = 0
 
-                    for(var i = 0; i < employees.length;i++){
-                        response[i].jobTitle = employees[i].jobTitle
-                        // response[i].reportTo = employees[i].reportTo
-						// response[i].isPeerBuddy = employees[i].isPeerBuddy
-						response[i]._id = employees[i]._id
-                        response[i].firstName = employees[i].firstName
-                        response[i].lastName = employees[i].lastName
-                        // response[i].email = employees[i].email
-						// response[i].workPhone = employees[i].workPhone
-						response[i].totalTasks = employees[i].tasks.length
-						var doneTasks = 0
-						var overDueTasks = 0
-
-						for(var j = 0; j < employees[i].tasks.length; j++){
-							if(employees[i].tasks[j].status == "Complete"){
-								doneTasks = doneTasks + 1
-							}else{
-								var dueDate = new Date(employees[i].tasks[j].dueDate) 
-								var currentDate = new Date()
-								if(currentDate > dueDate){
-									overDueTasks = overDueTasks + 1
-								}
-							}
+				for(var j = 0; j < employees[i].tasks.length; j++){
+					if(employees[i].tasks[j].status == "Complete"){
+						completedTasks = completedTasks + 1
+					}else{
+						var dueDate = new Date(employees[i].tasks[j].dueDate) 
+						var currentDate = new Date()
+						if(currentDate > dueDate){
+							overDueTasks = overDueTasks + 1
 						}
-						response[i].completedTasks = doneTasks
-						response[i].overDueTasks = overDueTasks
-						response[i].progress = doneTasks*100.0/completedTasks
+					}
+				}
+				response[i].completedTasks = completedTasks
+				response[i].overDueTasks = overDueTasks
 
-                    }
-            
+				if(employees[i].tasks.length != 0)
+					response[i].progress = completedTasks*100.0/employees[i].tasks.length
+				else{
+					response[i].progress = 100
+				}
+			}
+	
             res.status(200).send(response)
         }
             
